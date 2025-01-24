e511835e233f9a4480c7f775fec0c5e34d4a7e64
Nicu FLORICA (niq_ro) <nicu.florica@gmail.com>
2025-01-24 18:56:45 +0200

    Update Occupy_1.ino
    corrected program

--- Occupy_1.ino
+++ Occupy_1.ino
@@ -3,20 +3,9 @@
  * 0 - intial (momentary switch at D2__/__GND)
  * 0a - added on time in serial hh:mm:ss
  * 1 - changed to record just if a button was pushed (simulated sensor) for long time
- * 2 - changed momentary switch with Sharp IR sensor as at https://www.makerguides.com/sharp-gp2y0a710k0f-ir-distance-sensor-arduino-tutorial/
-       (if measured distance is lower than the threshold means activated sensor)
  */
 
-#include <SharpIR.h>  // https://github.com/guillaume-rico/SharpIR
-/* Model :
-  GP2Y0A02YK0F --> 20150
-  GP2Y0A21YK0F --> 1080
-  GP2Y0A710K0F --> 100500
-  GP2YA41SK0F  --> 430
-*/
-
-#define model 100500 // GP2Y0A710K0F
-#define senzor A0
+#define senzor 2
 #define LED0 13
 
 byte staresenzor1 = 1; // stare actuala senzor
@@ -27,37 +16,36 @@ unsigned long timp1;
 unsigned long timp2;
 unsigned long timp0;
 unsigned long pauza = 3000; // 
-int distanta = 0;   // in cm
-int distmin = 100;  // in cm
 
-SharpIR SharpIR(senzor, model);
 
 void setup() {
   Serial.begin(9600);
   Serial.println(" ");
-  Serial.println("ON/OFF control with Sharp IR sensor");
+  Serial.println("Occupy Area monitoring system with momentary button");
   Serial.println("Program by Nicu FLORICA (niq_ro)");
-  Serial.println("ver. 2 - 24.01.2025, Craiova");
+  Serial.println("ver. 1 - 23.01.2025, Craiova");
   Serial.println("---------------------");
   
   pinMode (LED0, OUTPUT);
   digitalWrite(LED0, 0);
+  pinMode (senzor, INPUT);
+  digitalWrite(senzor, HIGH);
 }
 
 
 void loop() {
-  distanta = SharpIR.distance();  // masoara distanta
-  if ((actionare == 0) and (distanta < distmin)) // este activat
+staresenzor1 = digitalRead(senzor);  // se citeste starea actuala a senzorului
+ if ((actionare == 0) and (staresenzor1 == LOW)) // este activat
    { 
-      Serial.println("Sesor was activated !");
+      Serial.println("Switch was activated !");
       timp2 = millis()/1000;  // store the time for first push record
       actionare = 1;
       delay(pauza);  // wait for debounce the false timmings
    }
-  distanta = SharpIR.distance();  // measurement of the distance
-  if (actionare == 1)
+staresenzor1 = digitalRead(senzor);  // se citeste starea actuala a senzorului
+ if (actionare == 1)
    { 
-  if (distanta < distmin)    // if is still activated
+   if (staresenzor1 == LOW) // daca inca este activat
    { 
       Serial.println("Switch is still activated !");
       actionare = 2; // good news
@@ -70,17 +58,17 @@ void loop() {
       actionare = 0;   // reset the status as not closed switch
    } 
    }
-  distanta = SharpIR.distance();  // masoara distanta
-  if (actionare == 2)
+staresenzor1 = digitalRead(senzor);  // se citeste starea actuala a senzorului
+ if (actionare == 2)
    { 
-   if (distanta > distmin)    // if is not activated
+   if (staresenzor1 == HIGH) // daca nu mai e activat
    { 
       Serial.println("--------------------");
-      Serial.println("Sensor was released !");
+      Serial.println("Switch was released !");
       //actionare = 0; // good news
       timp1 = millis()/1000;  // store the time for first push record
       timp0 = timp1 - timp2;
-      Serial.print("Sensor was activated for: ");
+      Serial.print("Switch was activated for: ");
       Serial.print((timp0/3600)/10);
       Serial.print((timp0/3600)%10);
       Serial.print("h:");
@@ -95,5 +83,5 @@ void loop() {
       actionare = 0;
    }
    }
-delay(500);
+delay(100);
 }  // sfarsit de program principal
